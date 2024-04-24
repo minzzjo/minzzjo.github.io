3969398512f51d527b1d7718f914c36f58735d4b
minxxjo <minxxjozz@gmail.com>
2024-04-24 22:33:09 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -17,6 +17,14 @@ document.addEventListener('scroll', () => {
 })
 
 // hide Arrow-up
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+})
 
 
 // apply Hambuger Menu in Mobile site
