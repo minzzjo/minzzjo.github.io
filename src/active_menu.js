f82a0900d57ae55dab67313fec6297cd9369e791
minxxjo <minxxjozz@gmail.com>
2024-04-25 21:18:12 +0900

    JS-Smooth Scroll

--- src/active_menu.js
+++ src/active_menu.js
@@ -18,7 +18,7 @@ function observerCallback(entries) {
     const index = sectionIds.indexOf(`#${entry.target.id}`);
     visibleSections[index] = entry.isIntersecting;
     selectLastOne = index === sectionIds.length - 1
-      && entry.isIntersecting && entry.intersectionRatio >= 0.99;
+      && entry.isIntersecting && entry.intersectionRatio >= 0.95;
   });
 
   const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
